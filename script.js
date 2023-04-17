99894e8472f631c98144580681a231bd415abda9
pro coder vinu <vkp4846@gmail.com>
2023-04-17 17:25:06 +0530

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".multiple", {
+    strings: ['Fullstack Web Developer', 'Youtuber',
+        'Software Developer'
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
